9ac0899fa181c6f50bffb191fcea735130810501
Ordav3 <Ordav3@gmail.com>
2023-06-29 20:21:10 +0300

    files update

--- app.js
+++ app.js
@@ -1,54 +1,21 @@
+require("./DB/connectToDb");
+// require("./primeryData/primeryCards")();
 const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const cors = require("cors");
-const apiRouter = require("./routes/api");
-const config = require("config");
-const initialData = require("./initialData/initialData");
-
 const app = express();
 
-console.log("file", config.get("file"));
-// console.log("anotherKey", config.get("anotherKey"));
+const usersRouter = require("./Routes/Users/userRouter");
+const cardsRouter = require("./Routes/Cards/cardsRouter");
+const chalk = require("chalk");
+const morgan = require("morgan");
+const cors = require("cors");
 
+app.use(morgan(chalk.cyan(":method :url :status :response-time ms")));
 app.use(cors());
-// app.use(
-//   cors({
-//     origin: "http://127.0.0.1:5500",
-//     optionsSuccessStatus: 200,
-//   })
-// );
-app.use(
-  logger(
-    ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"'
-  )
-);
-// app.use(
-//   logger((tokens, req, res) => {
-//     return [
-//       new Date().toISOString().replace("T", " "),
-//       tokens.method(req, res),
-//       tokens.url(req, res),
-//       tokens.status(req, res),
-//       tokens.res(req, res, "content-length"),
-//       "-",
-//       tokens["response-time"](req, res),
-//       "ms",
-//     ].join(" ");
-//   })
-// );
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
-app.use("/admin", express.static(path.join(__dirname, "admin")));
-// app.use("/biz", express.static(path.join(__dirname, "biz")));
-// app.use("/regular", express.static(path.join(__dirname, "regular")));
-initialData();
-app.use("/api", apiRouter);
-app.use((req, res, next) => {
-  res.status(404).json({ err: "page not found" });
-});
+app.use("/api/users", usersRouter);
+app.use("/api/cards", cardsRouter);
 
-module.exports = app;
+const PORT = 8181;
+app.listen(PORT, () =>
+  console.log(chalk.blueBright.bold(`server run on: http://localhost:${PORT}`))
+);
